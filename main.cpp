8de36f901791d2e37bff74241c53a2c20b82cbb2
freewins <freewings@sjtu.edu.cn>
2025-03-27 11:28:05 +0800

    Degree = 400.

--- main.cpp
+++ main.cpp
@@ -22,7 +22,7 @@ int main() {
       return strcmp(key_, other.key_) < 0;
     }
   };
-  BPlusTree<int,Key,800> tree("data");
+  BPlusTree<int,Key,400> tree("data");
   std::string op;
   char key[66];
   Key t;
